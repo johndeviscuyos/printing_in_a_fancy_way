5d7b113fbdcbacbb6d18abf852b10a1020d5d7f9
Devis <johndeviscuyos@gmail.com>
2024-03-23 20:29:38 +0800

    added the fancy print for dream job and name

--- fancy_print.py
+++ fancy_print.py
@@ -19,9 +19,14 @@ text_speech.say(speak_name)
 text_speech.runAndWait()
 #ask user their dream job
 dream_job= input("What is your dream job? ")
+
 #print a goodluck msg
 goodluck_message = (name + " your dream job is " + dream_job + ". Goodluck on achieving your dreams and keep striving")
 print (goodluck_message)
 text_speech.say(goodluck_message)
 text_speech.runAndWait()
+
 #print dream job and name in a fancy way
+art_text=pyfiglet.figlet_format(text= name+ " the "+ dream_job,
+                            font="slant")
+print (art_text)
