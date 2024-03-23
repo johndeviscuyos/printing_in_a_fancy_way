ccf65f1e558968ce1bf88a3e039577b11b7706e3
Devis <johndeviscuyos@gmail.com>
2024-03-23 20:10:08 +0800

    added input for dream job

--- fancy_print.py
+++ fancy_print.py
@@ -7,5 +7,6 @@ text_speech = pyttsx3.init()
 #ask user their name
 name = input("What is your name? ")
 #ask user their dream job
+dream_job= input("What is your dream job? ")
 #print a goodluck msg
 #print dream job and name in a fancy way
